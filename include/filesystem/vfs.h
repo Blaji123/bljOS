4a6f59abbad565c092baa36f718e947d92b07c76
Blaji123 <david_blajan523@yahoo.com>
2024-09-06 16:44:52 +0300

    added vfs.h

---
+++ include/filesystem/vfs.h
@@ -0,0 +1,162 @@
+#ifndef __BLJOS__FILESYSTEM__VFS_H
+#define __BLJOS__FILESYSTEM__VFS_H
+
+#include <common/types.h>
+#include <drivers/disk.h>
+
+#define FS_MODE_READ 0x01
+#define FS_MODE_WRITE 0x02
+#define FS_MODE_OPEN_EXISTING 0x00
+#define FS_MODE_CREATE_NEW 0x04
+#define FS_MODE_CREATE_ALWAYS 0x08
+#define FS_MODE_OPEN_ALWAYS 0x10
+#define FS_MODE_OPEN_APPEND 0x30
+
+#define O_ACCMODE 00000003
+#define O_RDONLY 00000000
+#define O_WRONLY 00000001
+#define O_RDWR 00000002
+#define O_CREAT 00000100  /* not fcntl */
+#define O_EXCL 00000200   /* not fcntl */
+#define O_NOCTTY 00000400 /* not fcntl */
+#define O_TRUNC 00001000  /* not fcntl */
+#define O_APPEND 00002000
+#define O_NONBLOCK 00004000
+#define O_DSYNC 00010000  /* used to be O_SYNC, see below */
+#define FASYNC 00020000   /* fcntl, for BSD compatibility */
+#define O_DIRECT 00040000 /* direct disk access hint */
+#define O_LARGEFILE 00100000
+#define O_DIRECTORY 00200000 /* must be a directory */
+#define O_NOFOLLOW 00400000  /* don't follow links */
+#define O_NOATIME 01000000
+#define O_CLOEXEC 02000000 /* set close_on_exec */
+#define __O_SYNC 04000000
+#define O_SYNC (__O_SYNC | O_DSYNC)
+#define O_PATH 010000000
+#define __O_TMPFILE 020000000
+#define O_TMPFILE (__O_TMPFILE | O_DIRECTORY)
+#define O_NDELAY O_NONBLOCK
+
+#define SEEK_SET 0  // start + offset
+#define SEEK_CURR 1 // current + offset
+#define SEEK_END 2  // end + offset
+
+namespace bljOS{
+    namespace filesystem{
+
+        typedef enum FS {FS_FATFS, FS_EXT2, FS_DEV, FS_SYS} FS;
+
+        typedef enum CONNECTOR{
+            CONNECTOR_AHCI,
+            CONNECTOR_DEV,
+            CONNECTOR_SYS
+        } CONNECTOR;
+
+        struct OpenFile;
+        struct MountPoint;
+
+        typedef int (*SpecialReadHandler)(OpenFile* fd, bljOS::common::uint8_t* out, bljOS::common::size_t limit);
+        typedef int (*SpecialWriteHandler)(OpenFile* fd, bljOS::common::uint8_t* in, bljOS::common::size_t limit);
+        typedef bljOS::common::size_t (*SpecialSeekHandler)(OpenFile* file, bljOS::common::size_t target, long int offset, int whence);
+        typedef int (*SpecialIoctlHandler)(OpenFile* fd, bljOS::common::uint64_t request, void* arg);
+//         typedef int (*SpecialStatHandler)(OpenFile* fd, stat* stat);
+        typedef bljOS::common::size_t (*SpecialMmapHandler)(bljOS::common::size_t addr, bljOS::common::size_t length, int prot, int flags, OpenFile* fd, bljOS::common::size_t pgoffset);
+        typedef bool (*SpecialDuplicate)(OpenFile* original, OpenFile* orphan);
+//         typedef int (*SpecialGetdents64)(OpenFile* fd, struct linux_dirent64* dirp, unsigned int count);
+        typedef bool (*SpecialOpen)(char* filename, OpenFile* fd, char** symlinkResolve);
+        typedef bool (*SpecialClose)(OpenFile* fd);
+        typedef bljOS::common::size_t (*SpecialGetFilesize)(OpenFile* fd);
+
+        struct VfsHandlers{
+            SpecialReadHandler read;
+            SpecialWriteHandler write;
+            SpecialSeekHandler seek;
+            SpecialIoctlHandler ioctl;
+//             SpecialStatHandler stat;
+            SpecialMmapHandler mmap;
+//             SpecialGetdents64 getdents64;
+            SpecialGetFilesize getFilesize;
+
+            SpecialDuplicate duplicate;
+            SpecialOpen open;
+            SpecialClose close;
+        };
+
+//         typedef bool (*MntStat)(MountPoint* mnt, char* filename, struct stat* target, char** symlinkResolve);
+//         typedef bool (*MntLstat)(MountPoint* mnt, char* filename, struct stat* target, char** symlinkResolve);
+
+        struct MountPoint{
+            MountPoint* next;
+
+            char* prefix;
+
+            bljOS::common::uint32_t disk;
+            bljOS::common::uint8_t partition;
+            CONNECTOR connector;
+
+            FS filesystem;
+
+            VfsHandlers* handlers;
+//             MntStat stat;
+//             MntLstat lstat;
+
+            bljOS::drivers::MBR_PARTITION mbr;
+            void* fsInfo;
+        };
+
+        struct OpenFile{
+            OpenFile* next;
+
+            int id;
+            int flags;
+            int mode;
+
+            bool closeOnExec;
+
+            char* dirname;
+
+            bljOS::common::size_t pointer;
+            bljOS::common::size_t tmp1;
+
+            VfsHandlers* handlers;
+
+            MountPoint* mountPoint;
+            void* dir;
+            void* fakefs;
+        };
+
+        class VirtualFileSystemController{
+            MountPoint* firstMountPoint;
+        public:
+            VirtualFileSystemController();
+
+            OpenFile* fsKernelOpen(char* filename, int flags, bljOS::common::uint32_t mode);
+            bool fsKernelClose(OpenFile* file);
+            int fsUserOpen(void* task, char* path, char* buf, int size);
+            int fsUserClose(void* task, int fd);
+            int fsUserSeek(void* task, bljOS::common::uint32_t fs, int offset, int whence);
+            OpenFile* fsUserGetNode(void* task, int fd);
+            OpenFile* fsUserDuplicateNode(void* taskPtr, OpenFile* original);
+            OpenFile* fsUserDuplicateNodeUnsafe(OpenFile* original);
+            bljOS::common::uint32_t fsRead(OpenFile* file, bljOS::common::uint8_t* out, bljOS::common::uint32_t limit);
+            bljOS::common::uint32_t fsWrite(OpenFile* file, bljOS::common::uint8_t* in, bljOS::common::uint32_t limit);
+            void fsReadFullFile(OpenFile* file, bljOS::common::uint8_t* out);
+            int fsReadLink(void* task, char* path, char* buf, int size);
+            bljOS::common::size_t fsGetFilesize(OpenFile* file);
+
+            char* fsStripMountPoint(const char* filename, MountPoint* mnt);
+            char* fsSanitize(char* prefix, char* filename);
+
+//             bool fsStat(OpenFile* fd, stat* target);
+//             bool fsStatByFilename(void* task, char* filename, stat* target);
+//             bool fsStatByFilename(void* task, char* filename, stat* target);
+
+            MountPoint* fsMount(char* prefix, CONNECTOR connector, bljOS::common::uint32_t disk, bljOS::common::uint8_t partition);
+            bool fsUnmount(MountPoint* mnt);
+            MountPoint* fsDetermineMountPoint(char* filename);
+            char* fsResolveSymlink(MountPoint* mnt, char* symlink);
+        };
+    }
+}
+
+#endif
---
+++ src/filesystem/vfs.cpp
@@ -0,0 +1,102 @@
+#include <filesystem/vfs.h>
+
+using namespace bljOS;
+using namespace bljOS::common;
+using namespace bljOS::drivers;
+using namespace bljOS::filesystem;
+
+VirtualFileSystemController::VirtualFileSystemController(){
+
+}
+
+OpenFile* VirtualFileSystemController::fsKernelOpen(char* filename, int flags, uint32_t mode){
+
+}
+
+bool VirtualFileSystemController::fsKernelClose(OpenFile* file){
+
+}
+
+int VirtualFileSystemController::fsUserOpen(void* task, char* path, char* buf, int size){
+
+}
+
+int VirtualFileSystemController::fsUserClose(void* task, int fd){
+
+}
+
+int VirtualFileSystemController::fsUserSeek(void* task, uint32_t fs, int offset, int whence){
+
+}
+
+OpenFile* VirtualFileSystemController::fsUserGetNode(void* task, int fd){
+
+}
+
+OpenFile* VirtualFileSystemController::fsUserDuplicateNode(void* taskPtr, OpenFile* original){
+
+}
+
+OpenFile* VirtualFileSystemController::fsUserDuplicateNodeUnsafe(OpenFile* original){
+
+}
+
+uint32_t VirtualFileSystemController::fsRead(OpenFile* file, uint8_t* out, uint32_t limit){
+
+}
+
+uint32_t VirtualFileSystemController::fsWrite(OpenFile* file, uint8_t* in, uint32_t limit){
+
+}
+
+void VirtualFileSystemController::fsReadFullFile(OpenFile* file, uint8_t* out){
+
+}
+
+int VirtualFileSystemController::fsReadLink(void* task, char* path, char* buf, int size){
+
+}
+
+size_t VirtualFileSystemController::fsGetFilesize(OpenFile* file){
+
+}
+
+char* VirtualFileSystemController::fsStripMountPoint(const char* filename, MountPoint* mnt){
+
+}
+
+char* VirtualFileSystemController::fsSanitize(char* prefix, char* filename){
+
+}
+
+// bool VirtualFileSystemController::fsStat(OpenFile* fd, stat* target){
+//
+// }
+
+// bool VirtualFileSystemController::fsStatByFilename(void* task, char* filename, stat* target){
+//
+//
+// }
+
+// bool VirtualFileSystemController::fsStatByFilename(void* task, char* filename, stat* target){
+//
+// }
+
+MountPoint* VirtualFileSystemController::fsMount(char* prefix, CONNECTOR connector, uint32_t disk, uint8_t partition){
+
+}
+
+bool VirtualFileSystemController::fsUnmount(MountPoint* mnt){
+
+}
+
+MountPoint* VirtualFileSystemController::fsDetermineMountPoint(char* filename){
+
+}
+
+char* VirtualFileSystemController::fsResolveSymlink(MountPoint* mnt, char* symlink){
+
+}
+
+
+
